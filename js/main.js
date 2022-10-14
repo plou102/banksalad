7a5104e0c78330e93ada7a3bca462936d79dcb54
jungmin Park <107393773+plou102@users.noreply.github.com>
2022-10-14 16:11:24 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,85 @@ for (let a = 0; a < blurName.length; a++) {
     blurName[a].classList.remove('hide');
   });
 }
+
+// card ranking swiper
+const swiper = new Swiper('.ranking-bg .swiper', {
+  direction: 'vertical',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: false
+});
+
+const cardRank = document.querySelector('.ranking-bg .swiper-wrapper');
+const cardRankAll = document.querySelector('.ranking-bg .swiper');
+
+cardRank.addEventListener('mouseover', function () {
+  swiper.slideTo(0, 0, false);
+  cardRankAll.classList.add('focus');
+  swiper.autoplay.stop();
+});
+cardRank.addEventListener('mouseout', function () {
+  cardRankAll.classList.remove('focus');
+  swiper.autoplay.start();
+});
+
+// visual swiper
+new Swiper('.visual .swiper', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: false,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.visual .swiper-pagination',
+    clickable: true
+  }
+});
+
+// pick swiper
+const swiperContents = document.querySelectorAll('.pick--contents .swiper');
+const swiperBtnPrev = document.querySelectorAll('.pick--contents .swiper-prev');
+const swiperBtnNext = document.querySelectorAll('.pick--contents .swiper-next');
+
+for (let i = 0; i < swiperContents.length; i++) {
+  if (i !== 0 && i !== 3) {
+    new Swiper(swiperContents[i], {
+      slidesPerView: 1,
+      centeredSlides: true,
+      loop: true,
+      navigation: {
+        prevEl: swiperBtnPrev[i],
+        nextEl: swiperBtnNext[i]
+      }
+    });
+  }
+}
+
+// review tab
+const reviewsTab = document.getElementsByName('reviews');
+const reviewClick = document.querySelectorAll('.bank--review .tabs--btn');
+const reviewContents = document.getElementsByName('review--tab');
+
+
+for (let p = 0; p < reviewsTab.length; p++) {
+  reviewClick[p].addEventListener('click', function () {
+    reviewsTab[p].checked = true;
+    reviewContents[p].checked = true;
+  });
+}
+
+// tip tab
+const tipsTab = document.getElementsByName('tips');
+const tipClick = document.querySelectorAll('.bank--tip .tabs--btn');
+const tipContents = document.getElementsByName('tips--tab');
+
+
+for (let n = 0; n < tipsTab.length; n++) {
+  tipClick[n].addEventListener('click', function () {
+    tipsTab[n].checked = true;
+    tipContents[n].checked = true;
+  });
+}
